eb6fb4ec2fe94feb3433a82d6e1107b307f098f4
Oles Melnik <olesmelnik24@gmail.com>
2021-08-30 11:58:27 +0300

    hamburger menu

--- src/components/ui/Menu.js
+++ src/components/ui/Menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,9 +6,12 @@ import {
     Link,
     NavLink,
 } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
 import "./menu.css";
+import CloseIcon from "@material-ui/icons/Close";
 
 const Menu = () => {
+    const [displayMenu, setDisplayMenu] = useState(false);
     return (
         <div>
             <nav>
@@ -34,7 +37,43 @@ const Menu = () => {
                         </NavLink>
                     </li>
                 </ul>
+                <div
+                    className="hamburger-block"
+                    onClick={() => {
+                        setDisplayMenu(!displayMenu);
+                    }}
+                >
+                    {displayMenu ? (
+                        <CloseIcon className="menu-icon" />
+                    ) : (
+                        <MenuIcon className="menu-icon" />
+                    )}
+                </div>
             </nav>
+            {displayMenu && (
+                <ul className="hamburger-menu">
+                    <li>
+                        <NavLink activeClassName="active" to="/" exact>
+                            Characters
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to="/episodes">
+                            Episodes
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to="/locations">
+                            Location
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName="active" to="/watchList">
+                            Watch Episode
+                        </NavLink>
+                    </li>
+                </ul>
+            )}
         </div>
     );
 };
